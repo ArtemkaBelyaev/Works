41628ff74cd3e3a21f649ab82b8fe833a0850f1a
ArtemkaBelyaev <130312648+ArtemkaBelyaev@users.noreply.github.com>
2024-09-04 12:45:54 +0300

    ok

---
+++ src/StreamTask/ForkJoinPoolExample.java
@@ -0,0 +1,40 @@
+package StreamTask;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+class FactorialTask extends RecursiveTask<Long> {
+    private final int n;
+
+    public FactorialTask(int n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Long compute() {
+        if (n <= 1) {
+            return 1L;
+        } else {
+
+            FactorialTask factorialTask = new FactorialTask(n - 1);
+
+            factorialTask.fork();
+
+            return n * factorialTask.join();
+        }
+    }
+}
+
+public class ForkJoinPoolExample {
+    public static void main(String[] args) {
+        int n = 12; // Вычисление факториала для числа 10
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        FactorialTask factorialTask = new FactorialTask(n);
+
+        long result = forkJoinPool.invoke(factorialTask);
+
+        System.out.println("Факториал " + n + "! = " + result);
+    }
+}
+
